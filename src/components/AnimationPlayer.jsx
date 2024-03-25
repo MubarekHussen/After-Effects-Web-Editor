ac1c35ec33b6003ec8199ca81061a1bbb6dec920
MubarekHussen <mubahussen2014@gmail.com>
2024-03-25 18:02:05 +0300

    feat: adding animation player

---
+++ src/components/AnimationPlayer.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
+import ExportButton from './ExportButton';
+
+const AnimationPlayer = ({ animationData }) => {
+  const container = useRef();
+  const animation = useRef();
+
+  useEffect(() => {
+    console.log('Received animationData:', animationData); // Log received data
+
+    if (animationData) {
+      if (animation.current) {
+        animation.current.destroy(); // Destroy previous animation if any
+      }
+      animation.current = lottie.loadAnimation({
+        container: container.current,
+        renderer: 'canvas',
+        loop: true,
+        autoplay: true,
+        animationData: animationData
+      });
+    }
+
+    return () => {
+      if (animation.current) {
+        animation.current.destroy();
+      }
+    };
+  }, [animationData]);
+
+  return (
+    <div className="p-6 mx-auto bg-white rounded-xl shadow-md flex flex-col items-center space-x-4">
+      <div ref={container} className="w-full h-96" />
+      <ExportButton animationData={animationData} /> {/* Include the ExportButton component */}
+    </div>
+  );
+};
+
+export default AnimationPlayer;
